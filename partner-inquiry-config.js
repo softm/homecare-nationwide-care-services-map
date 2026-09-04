226b8a8018f4b9835d6a0639215d89c942772ec3
softmnet <softmnet@gmail.com>
2026-09-04 11:58:12 +0900

    20260904_제휴문의_Web3Forms키연결

--- partner-inquiry-config.js
+++ partner-inquiry-config.js
@@ -1,7 +1,7 @@
 /** SOFTM-PARTNER-CONFIG START 날짜:20260904 : 두 지도에서 같은 수신처와 Web3Forms 공개 폼 키를 사용 */
 window.PARTNER_INQUIRY_CONFIG = {
     // Web3Forms에서 아래 수신 주소로 발급한 공개 Access Key를 입력합니다. 서버 비밀키는 넣지 않습니다.
-    accessKey: '',
+    accessKey: 'db6b8498-c7f5-4dd4-ade2-2239da75de3f', // SOFTM-PARTNER-KEY 날짜:20260904 : 발급받은 공개 폼 키로 제휴 문의 수신을 연결
     recipientEmail: 'softm@nate.com'
 };
 /** SOFTM-PARTNER-CONFIG END */
